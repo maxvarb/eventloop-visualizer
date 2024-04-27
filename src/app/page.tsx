d97759b80413457c23870fb9de5c0cd77f91fd9b
Maxim Shchamialeu <maxim.shchamialeu.varb@gmail.com>
2024-04-27 09:28:48 +0300

    feat: add code editor with syntax highlightning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import { CodeEditor } from '@/components/';
+
 export default function Home() {
 	return (
 		<main className="flex min-h-screen flex-col items-center justify-between p-24">
-			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+			<div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+				<CodeEditor className="w-[400px] h-[400px]" />
+			</div>
 		</main>
 	);
 }
