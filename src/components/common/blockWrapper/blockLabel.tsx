b7f9bdef5dbe128d7ebcbe9a2851c981fed44cfc
Maxim Shchamialeu <maxim.shchamialeu.varb@gmail.com>
2024-04-29 14:56:48 +0300

    feat: add blocks labels

---
+++ src/components/common/blockWrapper/blockLabel.tsx
@@ -0,0 +1,13 @@
+interface BlockLabelProps {
+	label: string;
+}
+
+export const BlockLabel = ({ label }: BlockLabelProps) => {
+	return (
+		<div className="z-50 -top-4 horizontal-center bg-[#434859] px-6 py-1.5 rounded-md border-2 border-[#575d71]">
+			<h1 className="text-white text-md font-semibold break-words whitespace-nowrap">
+				{label}
+			</h1>
+		</div>
+	);
+};
--- src/components/common/blockWrapper/index.tsx
+++ src/components/common/blockWrapper/index.tsx
@@ -1,19 +1,16 @@
 import { cn } from '@/lib/utils';
+import { BlockLabel } from './blockLabel';
 
 interface BlockWrapperProps {
 	className?: string;
 	children: React.ReactNode;
 	label?: string;
-	colSpan: number;
-	rowSpan: number;
 	noPadding?: boolean;
 }
 
 export const BlockWrapper = ({
 	className,
 	children,
-	colSpan,
-	rowSpan,
 	noPadding,
 	label,
 }: BlockWrapperProps) => {
@@ -22,10 +19,10 @@ export const BlockWrapper = ({
 			className={cn(
 				className,
 				!noPadding && 'p-2.5',
-				`col-span-${colSpan} row-span-${rowSpan}`,
-				'rounded-lg bg-[#23292e] text-white shadow-[10px_10px_12px_0px_rgba(0,0,0,0.67)]'
+				'relative rounded-lg bg-[#23292e] text-white shadow-[10px_10px_12px_0px_rgba(0,0,0,0.67)]'
 			)}
 		>
+			{label && <BlockLabel label={label} />}
 			{children}
 		</div>
 	);
